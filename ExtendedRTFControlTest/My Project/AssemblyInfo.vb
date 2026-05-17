450e5979e10daac09685ad36486427f1a5826fac
Hanibal1963 <18749753+Hanibal1963@users.noreply.github.com>
2026-05-17 11:16:43 +0200

    Version 1.2026.0517 veröffentlicht
    - Assembly- und Paketversionsnummern aktualisiert
    - Datei `ReleaseNotes.rtf` aus Projekt entfernt
    - Neuer Link zu GitHub-Seite in `vsixmanifest` hinzugefügt
    - Tags in `vsixmanifest` ergänzt
    - `ComVisible` in Testanwendung auf `False` gesetzt
    - GUID-Eintrag aus Testanwendung entfernt

--- ExtendedRTFControlTest/My Project/AssemblyInfo.vb
+++ ExtendedRTFControlTest/My Project/AssemblyInfo.vb
@@ -10,8 +10,7 @@
 <Assembly: System.Reflection.AssemblyProduct("ExtendedRTFControlTest")>
 <Assembly: System.Reflection.AssemblyCopyright("Copyright © 2026 by Andreas Sauer")>
 <Assembly: System.Reflection.AssemblyTrademark("")>
-<Assembly: System.Runtime.InteropServices.ComVisible(True)>
-<Assembly: System.Runtime.InteropServices.Guid("028b624a-4e40-44d2-9500-e9c95e6e8158")>
-<Assembly: Reflection.AssemblyVersion("1.2026.0507.7")>
-<Assembly: Reflection.AssemblyFileVersion("1.2026.0507.7")>
+<Assembly: System.Runtime.InteropServices.ComVisible(False)>
+<Assembly: Reflection.AssemblyVersion("1.2026.0517.1")>
+<Assembly: Reflection.AssemblyFileVersion("1.2026.0517.1")>
 <Assembly: System.Resources.NeutralResourcesLanguage("de-DE")>
